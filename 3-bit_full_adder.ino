56fb56fa1ac2f9cff74a2a7a60ad9265ebe956e3
jaeyoon-lee2 <54957190+jaeyoon-lee2@users.noreply.github.com>
2022-01-26 00:57:07 -0500

    Create 3-bit_full_adder.ino

---
+++ 3-bit_full_adder.ino
@@ -0,0 +1,42 @@
+/*
+  Created by: Jaeyoon Lee
+  Created on: Jan 2022
+  
+  3-bit Full Adder
+
+  Turn on the LEDs by converting from 0 to 2^(number of inputs)
+  	into binary every second.
+*/
+
+const int len = 3; // number of input A & B
+int A[len] = {5,6,7}; // input A pins
+int B[len] = {2,3,4}; // input B pins
+int N_INPUTS = 2*len;
+
+void setup()
+{
+  for (int i = 0; i < len; i++)
+  {
+    pinMode(A[i], OUTPUT);
+    pinMode(B[i], OUTPUT);
+  }
+}
+
+void loop()
+{
+  for (int i = 0; i < pow(2, N_INPUTS); i++)
+  {
+    bool bool_inputs[N_INPUTS];
+    for (int j = 0; j < N_INPUTS; j++)
+    {
+      bool_inputs[j] = ((i >> j) & 1) == 1;
+    }
+    
+    for (int idx = 0; idx < len; idx++)
+    {
+      digitalWrite(B[idx], bool_inputs[idx]);
+      digitalWrite(A[idx], bool_inputs[idx+len]);
+    }
+    delay(1000);
+  }
+}
