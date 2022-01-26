5111888d8ea915a3f6c3846d75a46f1ef004ae41
jaeyoon-lee2 <54957190+jaeyoon-lee2@users.noreply.github.com>
2022-01-26 00:59:33 -0500

    Create 4-bit_full_adder_subtractor.ino

---
+++ 4-bit_full_adder_subtractor.ino
@@ -0,0 +1,46 @@
+/*
+  Created by: Jaeyoon Lee
+  Created on: Jan 2022
+  
+  4-bit Adder/Subtractor
+
+  Turn on the LEDs by converting from 0 to 2^(number of inputs)
+  	into binary every 1/2 second.
+*/
+
+const int len = 4; // number of input A & B
+int A[len] = {6,7,8,9}; // input A pins
+int B[len] = {2,3,4,5}; // input B pins
+int M = 10; // +/- pin number
+int N_INPUTS = 2*len + 1;
+
+void setup()
+{
+  for (int i = 0; i < len; i++)
+  {
+    pinMode(A[i], OUTPUT);
+    pinMode(B[i], OUTPUT);
+  }
+  pinMode(M, OUTPUT);
+}
+
+void loop()
+{
+  for (int i = 0; i < pow(2, N_INPUTS); i++)
+  {
+    bool bool_inputs[N_INPUTS];
+    for (int j = 0; j < N_INPUTS; j++)
+    {
+      bool_inputs[j] = ((i >> j) & 1) == 1;
+    }
+    
+    digitalWrite(M, bool_inputs[N_INPUTS-1]);
+    
+    for (int idx = 0; idx < len; idx++)
+    {
+      digitalWrite(B[idx], bool_inputs[idx]);
+      digitalWrite(A[idx], bool_inputs[idx+len]);
+    }
+    delay(500);
+  }
+}
